aecf2607769b0085fd57bc7f53d95684e9aa94e9
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2023-06-17 05:01:16 +0900

    Create T1-25 다중조건.py

---
+++ Big_Data_Analyst/작업형1/T1-25 다중조건.py
@@ -0,0 +1,20 @@
+# 617 JungEun : 230609 FRI mission 23 (3) 
+# T1-25 STR slicing
+# basic1 데이터에서 f4가 E로 시작하면서 부산에 살고 20대인 사람 명 수
+
+
+# 1. Data and libraries
+import pandas as pd
+df = pd.read_csv('/kaggle/input/bigdatacertificationkr/basic1.csv')
+
+
+
+# 2.'f4' == E@@@ AND 'city' == 부산 AND 'age' == 20s
+cond1 = df['f4'].str[:1] == 'E'
+cond2 = df['city'] == '부산'
+cond3 = (df['age']>=20)&(df['age']<30)
+
+
+
+# 3. Count them
+print(len(df[cond1&cond2&cond3]))
