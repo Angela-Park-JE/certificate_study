731e550557c36d8a7d6c2298b91261304e1d6bbb
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2023-07-10 02:53:24 +0900

    Update previoustest-04-T1-3.py

--- Big_Data_Analyst/작업형1/previoustest-04-T1-3.py
+++ Big_Data_Analyst/작업형1/previoustest-04-T1-3.py
@@ -18,7 +18,7 @@ len(df[cond1&cond2])
 
 # 풀이
 
-# 풀이1 datatime 활용1 : dt.year, dt.month 사용
+# 풀이1 datatime 활용1
 import pandas as pd
 df = pd.read_csv("../input/big-data-analytics-certification-kr-2022/nf.csv")
 
@@ -59,7 +59,6 @@ df = pd.read_csv("../input/big-data-analytics-certification-kr-2022/nf.csv")
 cond1 = df['country'] == "United Kingdom"
 df['date_added'] = pd.to_datetime(df['date_added'])
 cond2 = df['date_added'].between('2018-1-1', '2018-1-31')
-
 print(len(df[cond1 & cond2]))
 
 
@@ -78,6 +77,31 @@ cond2 = df['date_added'].str.contains(str1)
 cond3 = df['date_added'].str.contains(str2)
 
 print(len(df[cond1 & cond2 & cond3]))
+
+
+
+# ---
+# (추가 문제) 4. 그런데 만약 'country'컬럼에 대소문자 함께 있고, 띄어쓰기가 있는 것도 있고 없는 것도 있다면?
+
+import pandas as pd
+df = pd.read_csv("../input/big-data-analytics-certification-kr-2022/nf.csv")
+
+# 띄어쓰기 제거
+df['country'] = df['country'].str.replace(' ','')
+
+# 소문자로 변경 - 아예 모두 변경해서 
+df['country'] = df['country'].str.lower()
+df['country']
+
+# 붙여진 상태로 검색 (한 곳만 있는 곳은 '==' 으로 검색이 됨)
+cond1 = df['country'] == "unitedkingdom"
+
 df['date_added'] = pd.to_datetime(df['date_added'])
-cond2 = df['date_added'].between('2018-1-1', '2018-1-31')
-print(len(df[cond1 & cond2]))
+df['year'] = df['date_added'].dt.year
+df['month'] = df['date_added'].dt.month
+
+
+cond2 = df['year'] == 2018
+cond3 = df['month'] == 1
+
+print(len(df[cond1 & cond2 & cond3]))
