853ca26b8d8341860d9934bc38503c5b028e5ee5
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2023-08-05 21:34:24 +0900

    Create previoustest-05-T1-1.py

---
+++ Big_Data_Analyst/작업형1/previoustest-05-T1-1.py
@@ -0,0 +1,25 @@
+# 617 JungEun : 230620 TUE mission 32 (1)
+# T1-1. 종량제 봉투 종류가 '규격봉투'이고, 종량제 봉투 용도가 '음식물쓰레기', 2L 가격 평균 
+# 가격 0은 제외하고, 반올림 후 '정수'로 출력
+
+# 1. 데이터, 라이브러리
+import numpy as np
+import pandas as pd
+df = pd.read_csv('/kaggle/input/mssion32-mission33-dataset/0620 - 미션32/5-1price.csv')
+
+
+
+# 2. conditions
+cond1 = df['종량제봉투종류'] == '규격봉투'
+cond2 = df['종량제봉투용도'] == '음식물쓰레기'
+cond3 = df['2ℓ가격'] != 0
+
+
+
+# 3. price
+avg = df[cond1&cond2&cond3]['2ℓ가격'].mean()
+print(int(round(avg)))
+
+
+# 118
+# 4분
