d441d981bae6b39a073e26fd3be986060f13e7a6
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2023-09-13 20:41:54 +0900

    Create previoustest-05-T1-2.py

---
+++ Big_Data_Analyst/작업형1/previoustest-05-T1-2.py
@@ -0,0 +1,32 @@
+# 617 JungEun : 230620 TUE mission 32 (2)
+# T1-2. 정상과 위험의 사람 수 차이의 절대값 -> '정수'로 출력
+# bmi(체질량지수): 몸무게(kg) / 키(m)의 제곱  
+# 정상체중: BMI 18.5이상 ~ 23미만 / 위험체중: BMI 23 이상 ~ 25미만  
+
+# 1. 데이터, 라이브러리
+import numpy as np
+import pandas as pd
+df = pd.read_csv('/kaggle/input/mssion32-mission33-dataset/0620 - 미션32/5-2bmi.csv')
+
+
+
+# 2. new col : '키'의 제곱
+# 값이 왜이렇게 큰가 생각하는데 2분정도 썼음
+df['bmi'] = (df['Weight']/(df['Height']/100)**2)
+
+
+
+# 3. conditions
+cond1 = (df['bmi']>=18.5)&(df['bmi']<23)
+cond2 = (df['bmi']>=23)&(df['bmi']<25)
+
+
+
+# 4. print the counts
+norm, warn = len(df[cond1]), len(df[cond2])
+print(int(abs(norm - warn)))
+
+
+
+# 144
+# 7분
