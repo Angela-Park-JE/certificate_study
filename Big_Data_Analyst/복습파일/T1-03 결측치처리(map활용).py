467c79ccb4b941de0b611099ce9953890462bb56
Jung-eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2025-08-22 22:56:32 +0900

    Update T1-03 결측치처리(map활용).py

--- Big_Data_Analyst/복습파일/T1-03 결측치처리(map활용).py
+++ Big_Data_Analyst/복습파일/T1-03 결측치처리(map활용).py
@@ -1,1 +1,24 @@
+import pandas as pd
+import numpy as np
 
+df = pd.read_csv('/kaggle/input/bigdatacertificationkr/basic1.csv')
+df.head(3)
+
+# EDA - 결측값 확인(비율 확인)
+df.info()
+
+# 80%이상 결측치 컬럼, 삭제
+df.isna().sum()/len(df)
+df.drop(columns = 'f3', inplace = True)
+df.head(3)
+
+# city 별 중앙값 및 대체
+df_med = df.groupby('city').median()
+print(df_med)
+
+fill_median_func = lambda m: m.fillna(m.median(), numeric_only = True) # numeric only 관련 옵션이 필요하게 된다.
+df_filled = df.groupby('city').apply(fill_median_func)
+df_filled.head(3)
+
+# f1 평균값 내어 결과 출력
+print(df_filled['f1'].mean())
