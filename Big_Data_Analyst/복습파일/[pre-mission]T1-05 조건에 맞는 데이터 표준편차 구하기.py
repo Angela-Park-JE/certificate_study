3ee7d5dbdde2861dad36b8738093ab7312c28d96
Jung-eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2025-08-27 00:02:27 +0900

    Update [pre-mission]T1-05 조건에 맞는 데이터 표준편차 구하기.py

--- Big_Data_Analyst/복습파일/[pre-mission]T1-05 조건에 맞는 데이터 표준편차 구하기.py
+++ Big_Data_Analyst/복습파일/[pre-mission]T1-05 조건에 맞는 데이터 표준편차 구하기.py
@@ -1,5 +1,13 @@
+# 250826: 조건에 맞는 데이터를 한정하여 표준편차를 구한 후
+# 두 표준편차의 절댓값을 구하기!
 import numpy as np
 import pandas as pd
 
 df = pd.read_csv('/kaggle/input/bigdatacertificationkr/basic1.csv')
-df.head(3)
+# df.head(3)
+
+df_enfj = df[df['f4'] == 'ENFJ'][['f4', 'f1']]
+df_infp = df[df['f4'] == 'INFP'][['f4', 'f1']]
+
+std_enfj, std_infp = df_enfj['f1'].std(), df_infp['f1'].std()
+print(np.abs(std_enfj - std_infp))
