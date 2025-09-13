eb65be08ad9d04b2da5d03d3dc291483434257e7
Jung-eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2025-09-13 23:16:26 +0900

    Update [pre-mission]T3-처치전후 혈압.py

--- Big_Data_Analyst/복습파일/[pre-mission]T3-처치전후 혈압.py
+++ Big_Data_Analyst/복습파일/[pre-mission]T3-처치전후 혈압.py
@@ -17,3 +17,18 @@ answer = round(np.mean(df['bp_gap']), 2)
 print(answer)
 
 
+# 2. 정규성 검정
+before, after = df['bp_pre'], df['bp_post']
+print(stats.kstest(after - before, 'norm'))  # 전-후
+
+# KstestResult(statistic=0.5486501019683699, pvalue=1.9590241771265486e-34)
+# 정규성 테스트 H0: 정규분포와 차이 없음 H1: 차이 있음
+# 0.05보다 작으면 귀무가설 기각, pvalue0.05 보다 작으므로 정규성과 유의미한 차이 있음
+
+# 지수표현 풀기
+# np.set_printoptions(suppress=True)
+# pd.options.display.float_format = '{:.6f}'.format   # 6째 자리까지 표현
+# pd.reset_option('display.float_format')
+
+
+# 3. 
